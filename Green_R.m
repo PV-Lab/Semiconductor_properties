28a6e794b645ae79a6dc634d336ac73480dfbe3f
maljensen <jensenma@mit.edu>
2015-08-25 10:02:57 -0400

    Updating scripts with more recent revisions

--- Green_R.m
+++ Green_R.m
@@ -1,10 +1,17 @@
-%This function was written on July 2, 2015 to determine the reflectivity of
-%silicon at a given temperature according to Green 2008. 
+%[R,sR] = Green_R(T,pump_lambda): This function was written on July 2, 2015
+%to determine the reflectivity of silicon at a given temperature according
+%to Green 2008. The inputs are sample temperature in degrees Kelvin and
+%incident wavelength in nm. The outputs are reflectivity (percentage) and
+%the standard deviation in the reflectivity (percentage) calculated
+%according to error propagation assuming no interacting terms.
+
+%Edited on July 21, 2015 to include a calculation of the error in the
+%reflectivity according to accuracy values in Green's paper. 
 
 %MAX WAVELENGTH = 1450 nm
 %MIN WAVELENGTH = 250 nm
 
-function [R] = Green_R(T,pump_lambda)
+function [R,sR] = Green_R(T,pump_lambda)
 
 load('C:\Users\Mallory\Documents\Lifetime spectroscopy\Experiments\wavelength_alpha_Calpha.mat'); 
 %column 4 of this file contains real refractive index
@@ -42,3 +49,11 @@ n_N2 = 1.0;
 
 %Reflectivity at this temperature
 R = (((n-n_N2)^2)+(k^2))/(((n+n_N2)^2)+(k^2)); 
+
+sn = (0.1/100)*n; %0.1% error according to Green
+sk = (4/100)*k; %4% error according to Green. This is dependent on wavelength we will accept 4% for most pump wavelengths. 
+
+dRdn = (-4*n_N2*((n_N2^2)+(k^2)-(n^2)))/(((n_N2^2)+(2*n_N2*n)+(k^2)+(n^2))^2);
+dRdk = (8*n_N2*k*n)/(((n_N2^2)+(2*n_N2*n)+(k^2)+(n^2))^2);
+
+sR = sqrt(((dRdn^2)*(sn^2))+((dRdk^2)*(sk^2))); 
--- Green_aBG.m
+++ Green_aBG.m
@@ -1,11 +1,18 @@
-%This function was written on July 2, 2015 to calculate the band to band
-%absorption as a function of temperature according to Green 2008. The
-%inputs are temperature (degrees K) and pump wavelength (nm)
+%[aBG,sBG] = Green_aBG(T,pump_lambda): This function was written on July 2,
+%2015 to calculate the band to band absorption as a function of temperature
+%according to Green 2008. The inputs are sample temperature in degrees
+%Kelvin and incident wavelength in nm. The outputs are band to band
+%absorption coefficient (/cm) and the standard deviation in the coefficient
+%(/cm) calculated according to error propagation assuming no interacting
+%terms.
+
+%Edited on July 21, 2015 to include a calculation of the error in the
+%reflectivity according to accuracy values in Green's paper. 
 
 %MAX WAVELENGTH = 1450 nm
 %MIN WAVELENGTH = 250 nm
 
-function [aBG] = Green_aBG(T,pump_lambda)
+function [aBG,saBG] = Green_aBG(T,pump_lambda)
 
 load('C:\Users\Mallory\Documents\Lifetime spectroscopy\Experiments\wavelength_alpha_Calpha.mat'); 
 %column 1 of this file contains wavelength in microns
@@ -27,3 +34,5 @@ b = C_alpha*T0;
 alpha0 = save_data(index,2);
 
 aBG = alpha0.*((T./T0).^b); 
+
+saBG = (4/100)*aBG; %4% error according to Green. This is dependent on wavelength we will accept 4% for most pump wavelengths. 
