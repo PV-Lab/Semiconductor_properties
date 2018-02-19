c69047cfdee2e6a8c93a3ce17c1a1deefc3d13a2
maljensen <jensenma@mit.edu>
2018-02-19 13:44:11 -0500

    modified the include the excess carrier density for Dorkel-Leturcq model

--- mobility.m
+++ mobility.m
@@ -25,10 +25,30 @@ SOFTWARE.
 %Mobility model from Dorkel and Leturqc as per Sinton Instruments WCT-120
 %Temperature Stage Report. Inputs include temperature in Kelvin, doping
 %concentration in atoms/cm3.
-function [mu_n,mu_p] = mobility(T,doping,type)
+function [mu_n,mu_p] = mobility(T,doping,type,delta_carrier)
 %Get some material parameters
 [Efi,Efv,p0,n0,Eiv] = adv_Model_gen(T,doping,type);
-ni2 = p0*n0; 
+
+%effective doping concentration
+if type == 'p'
+    if p0>doping
+        N = doping; %we don't want to account for the thermally excited carriers here
+    else
+        N = p0; 
+    end
+elseif type == 'n'
+    if n0>doping
+        N = doping; 
+    else
+        N = n0; 
+    end
+end
+
+%account for the injection level
+n = n0+delta_carrier;
+p = p0+delta_carrier; 
+
+ni2 = p*n; 
 %Assign constants
 mu_Lo_n = 1430; %cm2/Vs
 mu_Lo_p = 495; %cm2/Vs
@@ -43,10 +63,11 @@ mu_L_n = mu_Lo_n*((T/300)^(-alpha));
 mu_L_p = mu_Lo_p*((T/300)^(-alpha)); 
     
 %Calculate mu_I (equation 8), assuming N = doping concentration
-mu_I_n = (A_n*(T^(3/2))/doping)*((log(1+(B_n*(T^2)/doping))-(B_n*(T^2)/(doping+(B_n*(T^2)))))^(-1)); 
-mu_I_p = (A_p*(T^(3/2))/doping)*((log(1+(B_p*(T^2)/doping))-(B_p*(T^2)/(doping+(B_p*(T^2)))))^(-1)); 
+mu_I_n = (A_n*(T^(3/2))/N)*((log(1+(B_n*(T^2)/N))-(B_n*(T^2)/(N+(B_n*(T^2)))))^(-1)); 
+mu_I_p = (A_p*(T^(3/2))/N)*((log(1+(B_p*(T^2)/N))-(B_p*(T^2)/(N+(B_p*(T^2)))))^(-1)); 
 
 %Calculate mu_ccs (equation 9)
+%2e17 or 2e7?
 mu_ccs = ((2e17)*(T^(3/2))/sqrt(ni2))*((log(1+((8.28e8)*(T^2)*(ni2^(-1/3)))))^(-1)); 
 
 %Calculate X (equation 10)
